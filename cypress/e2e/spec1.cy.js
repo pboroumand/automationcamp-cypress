97b5f3761630ec2fe986c14447f1664b514fdb13
p.boroumand <b.boroumand89@gmail.com>
2025-09-19 21:14:30 +0200

    My Test Function

--- cypress/e2e/spec1.cy.js
+++ cypress/e2e/spec1.cy.js
@@ -1,5 +1,13 @@
+/// <reference types="cypress" />
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit ("www.play2.automationcamp.ir/index.html"),
+    console.log('Hello World!')
+  })
+  it.only('My Test', ()=>{
+    cy.visit ("www.play2.automationcamp.ir/index.html")
+    cy.get('#fname').type('Parni')
+    cy.get('#fname').should('have.value','Parni')
   })
 })
