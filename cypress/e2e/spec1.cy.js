40a72f0585a0ba0c808c81956443a365e377a79b
p.boroumand <b.boroumand89@gmail.com>
2025-09-19 22:08:46 +0200

    WatchForFileChanges

--- cypress/e2e/spec1.cy.js
+++ cypress/e2e/spec1.cy.js
@@ -7,7 +7,7 @@ describe('template spec', () => {
   })
   it.only('My Test', ()=>{
     cy.visit ("www.play2.automationcamp.ir/index.html")
-    cy.get('#fname').type('Parnia')
-    cy.get('#fname').should('have.value','Parnia')
+    cy.get('#fname').type('Parni Parni')
+    cy.get('#fname').should('have.value','Parni Parni')
   })
 })
