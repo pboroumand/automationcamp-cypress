33418bdd1f8138e7f225bda24f09e0dba90ea524
p.boroumand <b.boroumand89@gmail.com>
2025-09-21 02:04:28 +0330

    Session 2 - Last commit

--- cypress/e2e/Session 2.cy.js
+++ cypress/e2e/Session 2.cy.js
@@ -1,7 +1,15 @@
 /// <reference types="cypress" />
 
+//cy.visit ('https://datatables.net/examples/basic_init/scroll_xy.html')
+
 describe ('Locate Elements', ()=>{
     it ('cy get', ()=> {
-        cy.visit ('www.datatables.net/_/#/tests/')
+        cy.visit('https://play2.automationcamp.ir/index.html')
+        // cy.contains('.main > :nth-child(1)','This is your layout')
+        //cy.get('.main').find('h2').contains('This is').should('contain.text','This is your layout two')
+        cy.get('.main').within(()=>{
+            cy.get('h2').eq(0).should('contain.text','This is your layout two')
+        })
+        cy.get('td').filter('#td_id').should('contain.text','Smith')
     })
 })
