b43f8650eb888c499d20b481547bbdf87da48296
p.boroumand <b.boroumand89@gmail.com>
2025-09-26 02:52:36 +0330

    Alert

---
+++ cypress/e2e/Session 6.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe ('Intract with Elements', ()=>{
+    it('Alert',()=>{
+        cy.visit('https://play1.automationcamp.ir/expected_conditions.html')
+        cy.get('#min_wait').type('{backspace}1')
+        cy.get('#min_wait').should('have.value','1')
+        cy.get('#max_wait').type('{backspace}1')
+        cy.get('#max_wait').should('have.value','1')
+        cy.get('#alert_trigger').click().wait(1100)
+        cy.on('window:alert', function(message) {expect(message).eq('I am alerting you!')})
+        cy.get('#prompt_trigger').click().wait(1100)
+        cy.on('window:confirm', function(message){
+            expect(message).include("It's your life!")
+            return false
+        })
+        cy.get('#confirm_cancelled_badge').should('have.text','Cancelled')
+    })
+})
