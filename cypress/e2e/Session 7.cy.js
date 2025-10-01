6390f455e2d96c44a32579116d0911baaf78bf13
p.boroumand <b.boroumand89@gmail.com>
2025-10-01 07:59:33 +0330

    Each & Table

--- cypress/e2e/Session 7.cy.js
+++ cypress/e2e/Session 7.cy.js
@@ -6,4 +6,17 @@ describe('First spec', () => {
     cy.get("a[id*='js-link-box']").should('contain.text','日本語')
     cy.get("a[id*='js-link-box']").eq(-2).click()
   })
+  it('Each', () => {
+    cy.visit('https://www.wikipedia.org/')
+    cy.get("a[id*='js-link-box']").each(function($el,index,$list){
+      if (index===6){
+        cy.wrap($el).click()
+      }
+    })
+  })
+  it.only('Table', () => {
+    cy.visit('https://play2.automationcamp.ir/index.html')
+    cy.scrollTo('bottom')
+    cy.get('table th').eq(4).should('have.text','Occupation')
+  })
 })
