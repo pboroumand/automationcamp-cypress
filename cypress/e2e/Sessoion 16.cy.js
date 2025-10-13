4f89ef3c1fe0964ef0685d53bf35d20a1d8397e3
p.boroumand <b.boroumand89@gmail.com>
2025-10-13 23:45:00 +0330

    nested iframe

--- cypress/e2e/Sessoion 16.cy.js
+++ cypress/e2e/Sessoion 16.cy.js
@@ -1,15 +1,25 @@
 /// <reference types="cypress"/>
 
 describe ('Iframe', ()=>{
-    it.only('Iframe 1',()=>{
+    it('Iframe 1',()=>{
         cy.visit('https://play1.automationcamp.ir/frames.html')
         cy.get('#frame1').then(function($fr){
             const frcont = $fr.contents().find('body')
             cy.wrap(frcont).find('#click_me_1').click()
         })
     })
-    it('asserting',()=>{
-        cy.visit('')
+    it.only('nested iframe',()=>{
+        cy.visit('https://play1.automationcamp.ir/frames.html')
+        cy.get('#frame1').then(function($fr1){
+            const frcont1 = $fr1.contents().find('#frame2')
+            cy.wrap(frcont1).as('fr2ref')
+            cy.get('@fr2ref').then(function($fr2){
+                const frcont2= $fr2.contents().find('body')
+                cy.wrap(frcont2).find('#click_me_2').should('have.text','Click Me 2')
+                cy.wrap(frcont2).find('#click_me_2').click()
+                cy.wrap(frcont2).find('#click_me_2').should('have.text','Clicked')
+            })
+        })
     })
     it('asserting',()=>{
         cy.visit('')
